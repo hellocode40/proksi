1cc395b3cd92b232c1e17ece1d29b56bf9eaa1a4
hellocode40 <hellocode40@gmail.com>
2025-09-07 12:04:37 +0330

    change buckets

--- internal/metrics/http.go
+++ internal/metrics/http.go
@@ -21,37 +21,17 @@ func recordMetrics() {
 	}()
 }
 
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
-)
+var opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "myapp_processed_ops_total",
+	Help: "The total number of processed events",
+})
 
 func main() {
 	recordMetrics()
-
 }
 
-var buckets = []float64{
-	0.0005,
-	0.001, // 1ms
-	0.002,
-	0.005,
-	0.01, // 10ms
-	0.02,
-	0.05,
-	0.1, // 100 ms
-	0.2,
-	0.5,
-	1.0, // 1s
-	2.0,
-	5.0,
-	10.0, // 10s
-	15.0,
-	20.0,
-	30.0,
-}
+// 1ms to 10s
+var buckets = []float64{0.001, 0.002, 0.005, 0.01, 0.1, 1.0, 10.0}
 
 var (
 	HTTPReqCounter = promauto.NewCounterVec(prometheus.CounterOpts{
